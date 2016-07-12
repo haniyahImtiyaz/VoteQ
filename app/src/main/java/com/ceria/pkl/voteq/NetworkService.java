9e81ac35a270b886a06b09cc2e5bcba6c26604dc
Pandhu Weni <weniindya@gmail.com>
2016-07-12 15:04:46 +0700

    Ini Network Service

---
+++ app/src/main/java/com/ceria/pkl/voteq/NetworkService.java
@@ -0,0 +1,21 @@
+package com.ceria.pkl.voteq;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by pandhu on 12/07/16.
+ */
+public class NetworkService {
+    private final RequestQueue requestQueue;
+    private final Context context;
+
+    public NetworkService(Context context) {
+        this.context = context;
+        this.requestQueue = Volley. newRequestQueue(context);
+    }
+
+    
+}
