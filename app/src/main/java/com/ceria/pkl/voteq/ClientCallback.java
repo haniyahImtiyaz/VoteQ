8ae2523189ce07c42de42aa66a37347cb7732b6a
Pandhu Weni <weniindya@gmail.com>
2016-07-12 15:07:12 +0700

    tambah client callback

---
+++ app/src/main/java/com/ceria/pkl/voteq/ClientCallback.java
@@ -0,0 +1,9 @@
+package com.ceria.pkl.voteq;
+
+/**
+ * Created by pandhu on 12/07/16.
+ */
+public class ClientCallback {
+    void onSucceeded();
+    void onFailed();
+}
--- app/src/main/java/com/ceria/pkl/voteq/NetworkService.java
+++ app/src/main/java/com/ceria/pkl/voteq/NetworkService.java
@@ -17,5 +17,6 @@ public class NetworkService {
         this.requestQueue = Volley. newRequestQueue(context);
     }
 
-    
+
+
 }
