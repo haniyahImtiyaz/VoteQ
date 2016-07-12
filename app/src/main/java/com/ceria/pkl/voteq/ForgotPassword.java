ceade500a423d8f9a4ba66ed28d07f18adecc1a8
nurulhudaas <nrlhudaas@gmail.com>
2016-07-12 11:58:45 +0700

    menambah Forgot Password

---
+++ app/src/main/java/com/ceria/pkl/voteq/ForgotPassword.java
@@ -0,0 +1,13 @@
+package com.ceria.pkl.voteq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
