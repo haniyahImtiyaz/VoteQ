5676a043ca561a98f4ee4a8b91a001b99b3cbc1f
Haniyah Imtiyaz <haniyah.imtiyaz@gmail.com>
2016-07-20 16:04:23 +0700

    expand Grid

---
+++ app/src/main/java/com/ceria/pkl/voteq/ExpandGrid.java
@@ -0,0 +1,33 @@
+package com.ceria.pkl.voteq;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by win 8 on 7/20/2016.
+ */
+public class ExpandGrid extends GridView {
+
+        private android.view.ViewGroup.LayoutParams params;
+        private int old_count = 0;
+
+        public ExpandGrid(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            if (getCount() != old_count) {
+                old_count = getCount();
+                params = getLayoutParams();
+
+                setLayoutParams(params);
+
+            }
+
+            super.onDraw(canvas);
+        }
+
+    }
