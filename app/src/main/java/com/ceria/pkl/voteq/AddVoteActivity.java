78cdbb99b806763feacdaad29a44948605766c6e
Haniyah Imtiyaz <haniyah.imtiyaz@gmail.com>
2016-07-21 13:31:21 +0700

    keyboard set in create vote

--- app/src/main/java/com/ceria/pkl/voteq/AddVoteActivity.java
+++ app/src/main/java/com/ceria/pkl/voteq/AddVoteActivity.java
@@ -9,9 +9,12 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.paolorotolo.expandableheightlistview.ExpandableHeightListView;
@@ -57,6 +60,17 @@ public class AddVoteActivity extends AppCompatActivity implements ClientCallback
         expandableListView.setAdapter(listAdapterOption);
 
         expandableListView.setExpanded(true);
+
+        editTextOption.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    buttonAddOption.callOnClick();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         buttonAddOption.setOnClickListener(new View.OnClickListener() {
             @Override
